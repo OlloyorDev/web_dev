167b713648c512a5b9c00b78751fe5b364f8d250
onx. <olloyornodirov1999@mail.com>
2025-10-30 08:59:53 +0500

    modified

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -4,7 +4,6 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   connectivity_plus
-  firebase_core
   permission_handler_windows
   share_plus
   smart_auth
@@ -12,6 +11,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  flutter_local_notifications_windows
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
