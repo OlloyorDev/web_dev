cab7ea4e9365c7a36e14bf796907eff647d7cf1c
onx. <olloyornodirov1999@gmail.com>
2025-10-30 19:27:15 +0500

    pushed from new branch

--- lib/features/home/presentation/widgets/web/web_section_item.dart
+++ lib/features/home/presentation/widgets/web/web_section_item.dart
@@ -17,6 +17,7 @@ class WebSectionWrapper extends StatelessWidget {
     required this.sectionKey,
     required this.onTap,
   });
+
   @override
   Widget build(BuildContext context) => SliverToBoxAdapter(
         child: InkWell(
